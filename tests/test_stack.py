2fb68a3a6596f85be293ed25b91ff6e80bc5bb4f
gimntut <gimntut@list.ru>
2018-03-29 20:27:36 +0500

    Correct test

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -2,56 +2,51 @@ import pytest
 from pack.ng_stack import Layer, Stack
 
 
-def setup():
-    print("basic setup into module")
-    st = Stack()
-    l1 = Layer({"a": {"b": "c", "d": 1}, "e": [
-               2, 3, "f"], "g": "h", "i": None})
-    l2 = Layer({"a": {"j": "k"}})
-    l3 = Layer({"-a": None})
-    l3 = Layer({"e-": ["f"]})
-    st.push(l1)
-    r1 = {}
-    r2 = {}
-    r3 = {}
-
-
-import pytest
+@pytest.fixture
+def d0(request):
+    return {"a": {"b": "c", "d": 1}, "e": [
+        2, 3, "f"], "g": "h", "i": None}
 
 
 @pytest.fixture
-def layers(request):
+def stack(request, d0):
     st = Stack()
-    l1 = Layer({"a": {"b": "c", "d": 1}, "e": [
-               2, 3, "f"], "g": "h", "i": None})
-    st.push(l1)
-    return {
-        "st": st,
-        "l1": l1,
-        "l2": Layer({"a": {"j": "k"}}),
-        "l3": Layer({"-a": None}),
-        "l3": Layer({"e-": ["f"]}),
-        "r1": {},
-        "r2": {},
-        "r3": {},
-    }
+    l0 = Layer(d0)
+    st.push(l0)
+    return st
 
 
-def test_deleteproperty(layers):
-    print("test")
-    layers['st'].push(layers['l2'])
-    assert layers['st'].as_dict() == layers['r1']
+@pytest.fixture
+def l1(request):
+    return Layer({"a": {"j": "k"}})
 
 
-# def test_deleteitem():
-#     st.push(l3)
-#     assert st.as_dict == r2
+@pytest.fixture
+def l2(request):
+    return Layer({"-a": None})
 
 
-# def test_override():
-#     st.push(l4)
-#     assert st.as_dict == r3
+@pytest.fixture
+def l3(request):
+    return Layer({"e-": ["f"]})
 
 
-# def test_deleteproperty():
-#     assert 1 == 2
+def test_override(stack, l1, d0):
+    result = d0
+    result["a"] = {"j": "k"}
+    stack.push(l1)
+    assert stack.as_dict == result
+
+
+def test_deleteproperty(stack, l2, d0):
+    result = d0
+    del result["a"]
+    stack.push(l2)
+    assert stack.as_dict() == result
+
+
+def test_deleteitem():
+    result = d0
+    result["e"].remove("f")
+    st.push(l3)
+    assert st.as_dict == result
